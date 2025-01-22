b4296b21322ee8830134297e5b5c990260871ed9
dwhite02 <73139952+dwhite02@users.noreply.github.com>
2025-01-22 14:13:59 -0600

    Update sass import on how files are concatenated and import statements being on same line

--- index.js
+++ index.js
@@ -1,53 +1,81 @@
 'use strict';
 
-var fs			= require('fs');
-var path		= require('path');
-var gutil		= require('gulp-util');
-var throught	= require('through2');
+var fs = require('fs');
+var path = require('path');
+var gutil = require('gulp-util');
+var through = require('through2');
 
-function scssCombine(content, baseDir) {
-	var regex = /@import[^'"]+?['"](.+?)['"];?/g;
+// Function to find and return valid @import statements (ignores those inside comments)
+function findImports(cssText) {
+    // Step 1: Remove CSS block comments (/* ... */) and single-line comments (// ...)
+    const uncommentedCSS = cssText
+        .replace(/\/\*[\s\S]*?\*\//g, '')   // Remove block comments
+        .replace(/\/\/[^\n]*/g, '');        // Remove single-line comments
 
-	if (regex.test(content)) {
-		content	= content.replace(regex, function(m, capture) {
-			var parse	= path.parse(path.resolve(baseDir, capture));
-			var file	= parse.dir + '/_' + parse.name;
+    // Step 2: Match @import statements in the uncommented CSS
+    const regex = /@import\s*['"][^'"]+?['"]\s*;?/g;
+    const matches = [];
+    let match;
 
-			if (fs.existsSync(file + '.scss')) {
-				file	= file + '.scss';
-			} else if (fs.existsSync(file + '.scss.liquid')) {
-				file	= file + '.scss.liquid';
-			} else {
-				return '';
-			}
+    // Find all @import matches
+    while ((match = regex.exec(uncommentedCSS)) !== null) {
+        matches.push(match[0].trim());
+    }
 
-			return fs.readFileSync(file);
-		});
-	}
-
-	return content;
+    return matches;
 }
 
+// Main function to handle SCSS imports
+function scssCombine(content, baseDir) {
+    // Step 1: Find and collect valid @import statements
+    const validImports = findImports(content);
 
+    // Step 2: Replace each valid @import statement with the contents of the resolved file
+    validImports.forEach(function (importStatement) {
+        var regex = /@import\s*['"]([^'"]+)['"]\s*;?/;
+        var match = importStatement.match(regex);
+        if (match) {
+            var capture = match[1]; // Extract the import path
 
-module.exports	= function(opts) {
-	return throught.obj(function(file, enc, cb) {
-		if (file.isNull()) {
-			cb(null, file);
-			return;
-		}
+            var parse = path.parse(path.resolve(baseDir, capture));
+            var file = parse.dir + '/_' + parse.name;
 
-		if (file.isStream()) {
-			cb(new gutil.PluginError('gulp-scss-combine', 'Streaming not supported'));
-			return;
-		}
+            if (fs.existsSync(file + '.scss')) {
+                file = file + '.scss';
+            } else if (fs.existsSync(file + '.scss.liquid')) {
+                file = file + '.scss.liquid';
+            } else {
+                // If no file is found, skip this import statement
+                return;
+            }
 
-		if (path.basename(file.path).indexOf('_') === 0) {
-			return cb();
-		}
+            // Step 3: Replace the import statement with the file contents
+            content = content.replace(importStatement, fs.readFileSync(file, 'utf8'));
+        }
+    });
 
-		file.contents	= new Buffer(scssCombine(file.contents.toString(), path.dirname(file.path)));
+    return content;
+}
 
-		setImmediate(cb, null, file);
-	});
+module.exports = function (opts) {
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new gutil.PluginError('gulp-scss-combine', 'Streaming not supported'));
+            return;
+        }
+
+        if (path.basename(file.path).indexOf('_') === 0) {
+            return cb();
+        }
+
+        // Call scssCombine to process the file contents
+        file.contents = Buffer.from(scssCombine(file.contents.toString(), path.dirname(file.path)));
+
+        setImmediate(cb, null, file);
+    });
 };
